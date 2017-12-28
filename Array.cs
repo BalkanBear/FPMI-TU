3690b375d414cb415547c2c264f84c4628303a05
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2017-12-28 12:11:27 +0200

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input rows: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("Input cols: ");
+            int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows,cols];
+            for (int row = 0; row < rows; row++)
+            {
+                string rowValues = Console.ReadLine();
+                string[] values = rowValues.Split();
+                for (int col = 0; col < values.Length; col++)
+                {
+                    matrix[row,col] = int.Parse(values[col]);
+                }
+
+            }
+            int rowMax = 0, colMax = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row, col] > matrix[rowMax, colMax])
+                    {
+                        rowMax = row;
+                        colMax = col;
+                    }
+                    Console.WriteLine("Row = {0}, Col = {1}", rowMax + 1, colMax + 1);
+                }
+            }
+        }
+    }
+}
---
+++ Kih.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_projects
+{
+    class Kih
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int digit0 = number[0] - '0';
+            int digit1 = number[1] - '0';
+            int digit2 = number[2] - '0';
+
+            Console.WriteLine("Цифра 0: {0}, Цифра 1: {1} Цифра 2: {2}", digit0, digit1, digit2);
+            int sum = digit0 + digit1 + digit2;
+            char bukva = (char)('A' + sum - 1);
+
+            Console.WriteLine("Някой, чието име започва с буквата {0} си мисли за вас.", bukva);
+        }
+    }
+}
