e36e9e67b29bf49718997a22cc74de9532b22ca0
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2017-12-29 12:14:38 +0200

    Add files via upload

---
+++ Numb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our_number
+{
+    class Numb
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете вашето число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            var r = new Random();
+            int[] arr = new int[100];
+            int d = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(1, 101);
+                if (a==arr[i])
+                {
+                    d++;
+                }
+                Console.WriteLine(arr[i]);
+
+            }
+            Console.WriteLine("Числото ти се повтори {0} пъти.", d);
+        }
+    }
+}
---
+++ Palin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Palin
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                
+                Console.Write("Дума: ");
+                string s = Console.ReadLine();
+                bool isPalindrome = true;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] != s[s.Length - 1 - i])
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+
+                }
+                if (isPalindrome)
+                {
+                    Console.WriteLine("Палиндром!");
+                }
+                else
+                    Console.WriteLine("Думата НЕ е палиндром!");
+                break;
+            }
+            Console.WriteLine("Довиждане!");
+        }
+    }
+}
