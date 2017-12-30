9ee735d89447d66a61b41faf12b1ac85cb07093c
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2017-12-30 16:32:32 +0200

    Add files via upload

---
+++ Buyers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class Buyers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter demand: ");
+            decimal demand0 = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter demand unit: ");
+            decimal demandunit = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter initial cost: ");
+            decimal initialcost = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter cost unit: ");
+            decimal costunit = decimal.Parse(Console.ReadLine());
+            decimal a = -demandunit;
+            decimal b = costunit * demandunit + demand0;
+            decimal c = -costunit * demand0 - initialcost;
+            decimal priceunit = (-(b / (2 * a)));
+            decimal profit = a * priceunit * priceunit + b * priceunit + c;
+            Console.WriteLine("Priceunit is: {0}", priceunit);
+            Console.WriteLine("Max profit is: {0}", profit);
+        }
+    }
+}
---
+++ Rand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_number
+{
+    class Rand
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            int value = r.Next(1, 101);
+            int guess;
+            int attempt = 0;
+           
+
+            do
+            {
+                guess = int.Parse(Console.ReadLine());
+                if (value>guess)
+                {
+                    Console.WriteLine("Нагоре");
+                }
+                else if (value<guess)
+                {
+                    Console.WriteLine("Надолу");
+                }
+                ++attempt;
+            }
+            while (value != guess);
+            Console.WriteLine("Познахте от {0} опит.", attempt);
+        }
+    }
+}
