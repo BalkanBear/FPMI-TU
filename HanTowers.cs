7c64bd213849718ff70aadadc3d02a8322129f36
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2017-12-31 15:41:29 +0200

    Add files via upload

---
+++ HanTowers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    class HanTowers
+    {
+        static void Hanoj(int n, int a, int b, int c)
+        {
+            if(n>0)
+            {
+                Hanoj(n-1, a, c, b);
+                Console.WriteLine("Move from disk {0} to {1}", a,c);
+                Hanoj(n-1, b, a, c);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Брой дискове: ");
+            int n = int.Parse(Console.ReadLine());
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            Hanoj(n, a, b, c);
+            
+          
+        }
+    }
+}
---
+++ Sw.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+    class Sw
+    {
+        static void Main(string[] args)
+        {
+            int a = ReadInt("Въведете a: ");
+            int b = ReadInt("Въведете b: ");
+            Swap(ref a, ref b);
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+        }
+        static void Swap (ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        static int ReadInt(string prompt)
+        {
+            int a;
+            Console.Write(prompt);
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.Write("Въведете валидно цяло число: ");
+            }
+
+            return a;
+
+        }
+    }
+}
