d25ee189636fa08053d972f866c2bb5f9502344b
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2018-01-05 11:56:29 +0200

    Add files via upload

---
+++ LisNum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListNumbers
+{
+    class LisNum
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(i);
+            }
+            numbers[0] = 5;
+            numbers.Remove(7);
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ ResList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResizingLists
+{
+    class ResList
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            string consoleline = Console.ReadLine();
+            int n = int.Parse(consoleline);
+
+            string lineBreak = new string('-', 20);
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                var capacity = list.Capacity;
+                var count = list.Count;
+                Console.WriteLine("Total capacity of the list: {0}\nused:{1}\n{2}",
+                   capacity, count, lineBreak );
+                list.Add(i);
+            }
+        }
+    }
+}
