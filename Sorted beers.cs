d8d2f90ce2399c6d6b1ee05c792594044f866f9c
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2018-01-06 22:15:48 +0200

    Add files via upload

---
+++ Sorted beers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorted_beers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] beers = {"Zagorka", "Ariana", "Shumensko", "Astika", 
+                             "Kamenitza", "Bolqrka", "Amstel"};
+            Console.WriteLine("Unsorted: {0}", String.Join(", ", beers));
+            Array.Sort(beers);
+            Console.WriteLine("Sorted: {0}", String.Join(", ", beers));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    class Student
+    {
+        public string FirstName { get; set;  }
+        public string LastName { get; set;  }
+        public int Age { get; set;  }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student[] students =
+            {
+                new Student() {FirstName="Pesho", LastName="Ivanov", Age=20},
+                new Student() {FirstName="Ivan", LastName = "Petrov", Age=18},
+                new Student() {FirstName="Kiro", LastName="Nikolov", Age=21}
+            };
+            foreach (var s in students)
+            {
+                Console.WriteLine(s.FirstName + " " + s.LastName + " " + s.Age);
+            }
+
+            Array.Sort(students, (s1, s2) => s1.Age.CompareTo(s2.Age));
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var s in students)
+            {
+                Console.WriteLine(s.FirstName + " " + s.LastName + " " + s.Age);
+            }
+
+            Array.Sort(students, (s1, s2) => s1.LastName.CompareTo(s2.LastName));
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var s in students)
+            {
+                Console.WriteLine(s.FirstName + " " + s.LastName + " " + s.Age);
+            }
+        }
+    }
+}
