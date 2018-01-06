fc962b8a255f4a726a8e16d08ee18e686efdb17f
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2018-01-06 22:15:19 +0200

    Add files via upload

---
+++ BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] beers = {"Zagorka", "Ariana", "Shumensko", "Astika", 
+                             "Kamenitza", "Bolqrka", "Amstel"};
+            Array.Sort(beers);
+            string target = "Astika";
+            int index = Array.BinarySearch(beers, target);
+            Console.WriteLine("{0} is found at index {1}.", target, index);
+
+            target = "Heineken";
+            index = Array.BinarySearch(beers, target);
+            Console.WriteLine("{0} is not found(index={1}).", target, index);
+        }
+    }
+}
---
+++ DivideSorting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide_Sorting
+{
+    public class SortingArrays
+    {
+        private class NumbersComparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return (x % 5).CompareTo(y % 5);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 5, 6, 10, 11 };
+            Array.Sort(numbers, new NumbersComparer());
+            Console.WriteLine("Numbers sorted by modul 5: ");
+            foreach (var s in numbers)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            Array.Sort(numbers, (x,y) => (y % 6).CompareTo (x % 6));
+            Console.WriteLine("Numbers sorted descending: ");
+            foreach (var s in numbers)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
