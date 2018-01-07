767b12a1609d4db27ee789b82c1293a849c6fc0a
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2018-01-08 00:41:08 +0200

    Add files via upload

---
+++ MethArr.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Array
+{
+    class MethArr
+    {
+        static void PrintArray (int[] arr)
+        {
+            foreach (var value in arr)
+            {
+                Console.Write(value);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        static void Example (int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] + 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 5 };
+            PrintArray(arr);
+            Example(arr);
+            PrintArray(arr);
+        }
+    }
+}
