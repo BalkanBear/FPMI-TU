06393ee0a413831d2201d3b299f735bc889b2273
BalkanBear <33843728+BalkanBear@users.noreply.github.com>
2018-03-13 20:28:16 +0200

    Add files via upload

---
+++ Prob 2 - Copy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    class Program
+    {
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        static void Main(string[] args)
+        {
+            string s = "Bonbon and Bonboniera";
+            Console.WriteLine(Reverse(s));
+        }
+    }
+}
